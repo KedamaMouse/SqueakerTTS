3ffd987602b7ff638a7aa0cca4e055715eb1a380
Kedama Mouse <2153956+KedamaMouse@users.noreply.github.com>
2022-07-04 20:17:37 -0500

    more tetorial/add .dotnet project

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,6 +1,17 @@
 
 import * as React from 'react';
 
+
+import { ConnectionBuilder, Connection } from "electron-cgi";
+
 export const App = () => {
+    const [connection, setConnection] = React.useState<Connection>();
+
+    if(!connection)
+    {
+        
+        setConnection(new ConnectionBuilder().connectTo("dotnet", "run", "--project", "./core/Core").build());
+    }
+
     return <div>Hello Mom!</div>;
 };
