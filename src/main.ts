573c09c876b3825a4d3e4745d771d6e9c973acaa
Kedama Mouse <2153956+KedamaMouse@users.noreply.github.com>
2022-09-24 16:48:12 -0500

    trigger an exe on speak start/stop

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,11 @@
 const url = require("url");
 const path = require("path");
-const fs = require('fs');
 
 import { app, BrowserWindow, Menu } from "electron";
 import { ConnectionHandler } from "./ConnectionHandler";
 import { GetMenuTemplate } from "./electronMenu";
 
 let window: BrowserWindow | null;
-
-export let exepath=path.join(path.dirname(app.getPath("exe")),"bin\\SqueakerTTSCmd.exe");;
-if(!fs.existsSync(exepath)) //different file path when not packaged.
-{
-  exepath=path.join(__dirname,"bin\\SqueakerTTSCmd.exe");
-}
-if(!fs.existsSync(exepath))
-{
-  app.quit();
-}
 
 app.commandLine.appendSwitch('disable-renderer-backgrounding');
 
